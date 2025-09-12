e0416785ba47b0dcc9bf52de3a6ff2ac883faacd
Cacie Prins <cacie@cypress.io>
2025-09-12 11:20:16 -0400

    update snapshots

--- cli/__snapshots__/spawn_spec.ts.js
+++ cli/__snapshots__/spawn_spec.ts.js
@@ -5,6 +5,7 @@ exports['lib/exec/spawn .start forces colors and streams when supported 1'] = {
   'FORCE_STDIN_TTY': '1',
   'FORCE_STDOUT_TTY': '1',
   'FORCE_STDERR_TTY': '1',
+  'DBUS_SESSION_BUS_ADDRESS': 'disabled:',
 }
 
 exports['lib/exec/spawn .start does not force colors and streams when not supported 1'] = {
@@ -13,6 +14,7 @@ exports['lib/exec/spawn .start does not force colors and streams when not suppor
   'FORCE_STDIN_TTY': '0',
   'FORCE_STDOUT_TTY': '0',
   'FORCE_STDERR_TTY': '0',
+  'DBUS_SESSION_BUS_ADDRESS': 'disabled:',
 }
 
 exports['lib/exec/spawn .start detects kill signal exits with error on SIGKILL 1'] = `
